5c995f65597fecf7227c80e517345385ecb1a969
Czyryca <k.czyryca@gmail.com>
2016-10-07 01:07:16 -0400

    Added timing statements

--- Driver.cpp
+++ Driver.cpp
@@ -1,14 +1,28 @@
 #include "Node.h"
 #include "Tree.h"
 #include <iostream>
+#include <stdio.h>
+#include <ctime> //used to measure runtime
 
  
 using namespace std;
 
 int main(){
-
-  Tree *test = new Tree(8);
-  test->retAnalysisIter(true, &(test->root));
-  cout << "Tree value: " << test->value << endl;
-  return 0;
+  for (int i = 10; i <= 20; i++){
+    Tree *test = new Tree(i);
+    clock_t begin = clock();
+    test->retAnalysisIter(true, &(test->root));
+    clock_t end = clock();
+    printf("Iterative on %d levels: got %d in %f seconds.\n",
+            i,test->value,double(end-begin)/CLOCKS_PER_SEC);
+    }
+  for (int i = 10; i <= 20; i++){
+    Tree *test = new Tree(i);
+    clock_t begin = clock();
+    test->retAnalysisRecursive();
+    clock_t end = clock();
+    printf("Recursive on %d levels: got %d in %f seconds.\n",
+            i,test->value,double(end-begin)/CLOCKS_PER_SEC);
+    }
+    return 0;
 }
